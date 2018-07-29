dcdc5b74dac864149ce4b609a8d3903784566cfd
Tomoki <sttmk.yamamoto@gmail.com>
2018-07-29 20:28:02 +0900

    任意のコメント

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -8,14 +8,40 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
-    # redirect_toメソッドを用いて、自動的に投稿一覧ページに転送されるようにしてください
-    def create
-      @post=Post.new(content: params[:content])
-      @post.save
+    @post = Post.new(content: params[:content])
+    # 保存に成功した場合は投稿一覧ページ、保存に失敗した場合は新規投稿ページが表示されるようにif-else文を追加してください
+    if @post.save
+      flash[:notice]="投稿を作成しました！"
       redirect_to("/posts/index")
+    else
+      render("posts/new")
     end
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice]="投稿を削除しました！"
+    redirect_to("/posts/index")
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  # contentカラムに対して、空の投稿を制限するバリデーションを作成してください
+  validates :content, {presence: true, length: {maximum: 140}}
 end
