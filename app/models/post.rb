e03e2751bde91b357258d7085ec0bbdcee970211
Tomoki <sttmk.yamamoto@gmail.com>
2018-08-01 11:18:17 +0900

    投稿とユーザーの紐付け

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   # contentカラムに対して、空の投稿を制限するバリデーションを作成してください
   validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id,{presence: true}
+
+  def user
+    return User.find_by(id:self.user_id)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_30_012647) do
+ActiveRecord::Schema.define(version: 2018_07_31_035002) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
